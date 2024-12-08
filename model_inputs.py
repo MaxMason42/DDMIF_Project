3dcde4752778394a8a1a6cca4533152147cea344
MaxMason42 <maximilian.mason@columbia.edu>
2024-12-08 00:30:52 -0500

    Code

--- model_inputs.py
+++ model_inputs.py
@@ -1,3 +1,27 @@
+# Based on the papers:
+# 
+# @article{wood2021trading,
+#   title={Trading with the Momentum Transformer: An Intelligent and Interpretable Architecture},
+#   author={Wood, Kieran and Giegerich, Sven and Roberts, Stephen and Zohren, Stefan},
+#   journal={arXiv preprint arXiv:2112.08534},
+#   year={2021}
+# }
+# 
+# @article {Wood111,
+# 	author = {Wood, Kieran and Roberts, Stephen and Zohren, Stefan},
+# 	title = {Slow Momentum with Fast Reversion: A Trading Strategy Using Deep Learning and Changepoint Detection},
+# 	volume = {4},
+# 	number = {1},
+# 	pages = {111--129},
+# 	year = {2022},
+# 	doi = {10.3905/jfds.2021.1.081},
+# 	publisher = {Institutional Investor Journals Umbrella},
+# 	issn = {2640-3943},
+# 	URL = {https://jfds.pm-research.com/content/4/1/111},
+# 	eprint = {https://jfds.pm-research.com/content/4/1/111.full.pdf},
+# 	journal = {The Journal of Financial Data Science}
+# }
+
 
 import numpy as np
 import sklearn.preprocessing
@@ -46,7 +70,7 @@ class ModelFeatures:
             self,
             df,
             total_time_steps,
-            start_boundary=2010,
+            start_boundary=2017,
             test_boundary=2022,
             test_end=2023,
             changepoint_lbws=None,
